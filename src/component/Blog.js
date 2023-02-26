4c6c75cd53a5c31cca5cf44fa6483536838c14b5
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-02-26 23:22:39 +0600

    style added

--- src/component/Blog.js
+++ src/component/Blog.js
@@ -1,74 +1,114 @@
-import React, { useState, useEffect } from "react";
-import { Pagination } from "react-bootstrap";
+import { Container, Row, Col, ListGroup, Pagination, Form, Card } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
 
-function BlogPosts() {
+
+
+function Blog() {
   const [posts, setPosts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(10);
-  const [sortOrder, setSortOrder] = useState("asc");
-  const [searchTerm, setSearchTerm] = useState("");
-	 let limit = 10;
+  const [sortOrder, setSortOrder] = useState('asc');
+  const [sortBy, setSortBy] = useState('title');
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((data) => setPosts(data))
-      .catch((error) => console.error(error));
+    const fetchPosts = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = await response.json();
+      setPosts(data);
+    };
+
+    fetchPosts();
   }, []);
 
-  // Sort posts by title or body
-  const sortedPosts = [...posts].sort((a, b) => {
-    const isReversed = sortOrder === "asc" ? 1 : -1;
-    return isReversed * a.title.localeCompare(b.title);
+  const pageSize = 10;
+  const indexOfLastPost = currentPage * pageSize;
+  const indexOfFirstPost = indexOfLastPost - pageSize;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handleSort = (event) => {
+    setSortOrder(event.target.value);
+  };
+
+  const handleSortBy = (event) => {
+    setSortBy(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const sortedPosts = currentPosts.sort((a, b) => {
+    if (sortOrder === 'asc') {
+      return a[sortBy].localeCompare(b[sortBy]);
+    } else {
+      return b[sortBy].localeCompare(a[sortBy]);
+    }
   });
 
-  // Filter posts by title or body
-  const filteredPosts = sortedPosts.filter(
-    (post) =>
-      post.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      post.body.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  // Get current posts
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
-
-  // Change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
-  // Toggle sort order
-  const toggleSortOrder = () =>
-    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  const filteredPosts = sortedPosts.filter((post) => {
+    const titleMatch = post.title.toLowerCase().includes(searchTerm.toLowerCase());
+    const bodyMatch = post.body.toLowerCase().includes(searchTerm.toLowerCase());
+    return titleMatch || bodyMatch;
+  });
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Search by title or body"
-        value={searchTerm}
-        onChange={(event) => setSearchTerm(event.target.value)}
-      />
-      <button onClick={toggleSortOrder}>
-        {sortOrder === "asc" ? "Sort Descending" : "Sort Ascending"}
-      </button>
-      <div className="row card">
-        {currentPosts.map((post) => (
-          
-           <div className="col-lg-3 col-md-6 col-sm-12">
-             <h3>{post.title}</h3>
-            <p>{post.body}</p>
-           </div>
-          
-        ))}
-      </div>
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={filteredPosts.length}
-        paginate={paginate}
-      />
-    </div>
+    <Container>
+    <Row>
+      <Col>
+        <Form>
+          <Form.Group controlId="search" className='mb-5' style={{ width: '50%', alignItems: 'center', justifyContent: 'center', marginLeft:'250px', marginTop:'20px'}}>
+            <Form.Control type="text" placeholder="Search" value={searchTerm} onChange={handleSearch} />
+          </Form.Group>
+        </Form>
+      </Col>
+    </Row>
+    <Row xs={1} md={2} className="g-4">
+      {filteredPosts.map((post) => (
+         <Col>
+         <Card>
+           <Card.Body>
+             <Card.Title>{post.title}</Card.Title>
+             <Card.Text>
+             {post.body}
+             </Card.Text>
+           </Card.Body>
+         </Card>
+       </Col>
+      ))}
+    </Row>
+    
+    <Row>
+      <Col>
+        <Pagination style={{marginLeft:'350px', marginTop:'20px'}}>
+          {[...Array(Math.ceil(posts.length / pageSize)).keys()].map((pageNumber) => (
+            <Pagination.Item
+              key={pageNumber + 1}
+              active={pageNumber + 1 === currentPage}
+              onClick={() => handlePageChange(pageNumber + 1)}
+            >
+              {pageNumber + 1}
+            </Pagination.Item>
+          ))}
+        </Pagination>
+      </Col>
+      <Col>
+          {/* <Form.Group controlId="sort">
+          <Form.Label>Sort by:</Form.Label>
+          <Form.Control as="select" value={sortBy} onChange={handleSortBy}>
+              <option value="title">Title</option>
+              <option value="body">Body</option>
+              </Form.Control>Form.Control
+            </Form.Group> */}
+            
+             
+              </Col>
+      </Row>
+      </Container>
+     
   );
-}
-
-export default BlogPosts;
+          }
+  export default Blog;
